46fc8ee4a0ae9c25228c6e12a78b0ed68db78efc
Fahad Mashi <Fahadmashi@hotmail.com>
2023-11-09 10:51:30 +0300

    A- Schema

--- qrify/urls.py
+++ qrify/urls.py
@@ -4,10 +4,10 @@ from .import views
 app_name = 'qrify'
 
 urlpatterns = [
-    path('qrify/', views.qr_code_list, name='qr_code_list'),
-    path('qrify/create/url/', views.create_url_qr_code, name='create_url_qr_code'),
-    path('qrify/create/text/', views.create_text_qr_code, name='create_text_qr_code'),
-    path('qrify/<int:qr_code_id>/', views.qr_code_detail, name='qr_code_detail'),
+    # path('qrify/', views.qr_code_list, name='qr_code_list'),
+    # path('qrify/create/url/', views.create_url_qr_code, name='create_url_qr_code'),
+    # path('qrify/create/text/', views.create_text_qr_code, name='create_text_qr_code'),
+    # path('qrify/<int:qr_code_id>/', views.qr_code_detail, name='qr_code_detail'),
     
     # api
     #Create 
